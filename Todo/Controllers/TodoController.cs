324decdfa127e9fb08a2b252625eec453ed5144b
carlosalbertofilho <carlosalberto_filho@outlook.com>
2024-03-07 14:58:35 -0300

    #Controller:
    Created TodoController class and added basic functionality
    
    Added the `Microsoft.AspNetCore.Mvc` and `Todo.Models` namespaces to the `TodoController.cs` file. Created a new `TodoController` class that inherits from `ControllerBase` and is decorated with the `ApiController` attribute. The `Route` attribute has been added to the `TodoController` class with the value "[controller]". A constructor for the `TodoController` class has been added, which takes an `ILogger<TodoController>` parameter and stores it in a private readonly field `_logger`. A `Get` method has been added to the `TodoController` class, decorated with the `HttpGet` attribute and a name of "GetTodo". This method logs an informational message and returns an empty list.

---
+++ Todo/Controllers/TodoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Todo.Models;
+
+namespace Todo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoController(ILogger<TodoController> logger) : ControllerBase
+    {
+        private readonly ILogger<TodoController> _logger = logger;
+
+        [HttpGet(Name = "GetTodo")]
+        public IEnumerable<TodoModel> Get()
+        {
+            _logger.Log(LogLevel.Information, "Getting todo list");
+            return [];
+        }
+    }
+}
