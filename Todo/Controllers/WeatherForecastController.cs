ecef317e42d73f4ab9a1c8b09533fd491d7be3ac
carlosalbertofilho <carlosalberto_filho@outlook.com>
2024-03-07 13:55:27 -0300

    #init:
    "Added new 'Todo' project and weather forecast feature"
    
    Added a new project named "Todo" to the solution file `BaltaAspNet.sln` with configurations for Debug and Release modes. Two new configuration files `appsettings.Development.json` and `appsettings.json` were added for logging levels and allowed hosts. The `Program.cs` file was updated to setup a new web application. A new project file `Todo.csproj` was created with .NET 8.0 as the target framework and a package reference to Swashbuckle.AspNetCore version 6.4.0. A new HTTP file `Todo.http` was created for a GET request to the weather forecast endpoint. A new controller `WeatherForecastController.cs` and a new model `WeatherForecast.cs` were added to the Todo project for a weather forecast feature. The `launchSettings.json` file was updated for running the application in development mode with the launch URL set to "swagger".

---
+++ Todo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Todo.Models;
+
+namespace Todo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController(ILogger<WeatherForecastController> logger) : ControllerBase
+    {
+        private static readonly string[] Summaries =
+        [
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        ];
+
+        private readonly ILogger<WeatherForecastController> _logger = logger;
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.Log(LogLevel.Information, "Getting weather forecast");
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
